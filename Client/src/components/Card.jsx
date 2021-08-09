978aba5a8e5ebfeda0ad66d09f4d8d3e262fdb88
Mauro Vera <81174890+Mauro069@users.noreply.github.com>
2021-08-09 19:37:55 -0300

    Commit Inicial

---
+++ Client/src/components/Card.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Card.css";
+import boton from "./boton.png";
+
+export default function Card({ min, max, name, img, onClose, id, humedad }) {
+  console.log(humedad);
+  return (
+    <div className="card__conteiner">
+      <div className="img__conteiner">
+        <img
+          className="img"
+          src={"http://openweathermap.org/img/wn/" + img + "@2x.png"}
+          alt="Img"
+        />
+      </div>
+
+      <div className="derecha__conteiner">
+        <div className="text__conteiner">
+          <div>
+            <h5 className="city__name">{name}</h5>
+          </div>
+          <div className="data__conteiner">
+            <div className="minmax__conteiner">
+              <p className="minmax">Min</p>
+              <p className="temp">{min}°</p>
+            </div>
+            <div className="min-max-conteiner">
+              <p className="minmax">Max</p>
+              <p className="temp">{max}°</p>
+            </div>
+          </div>
+        </div>
+        <div className="boton__conteiner">
+          <button onClick={onClose}>
+            <img className="boton" src={boton} alt="boton" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ Client/src/components/SearchBar.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import './SearchBar.css'
+
+export default function SearchBar({onSearch}) {
+  const [city, setCity] = useState('');
+  return (
+    <form className='form-conteiner' onSubmit={(e) => {
+      e.preventDefault();
+      onSearch(city);
+      setCity('');
+    }}>
+      <input className = 'elinput'
+        type="text"
+        placeholder="Clima en..."
+        value = {city}
+        onChange = {e => setCity(e.target.value)}
+      />
+      <input className = 'elboton' type="submit" value="Agregar" />
+    </form>
+  ); 
+}
