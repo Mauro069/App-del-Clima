9063c3006d225b865106fb139e891fbe067cc0b2
Mauro Vera <81174890+Mauro069@users.noreply.github.com>
2021-08-12 20:20:00 -0300

    Saque los warnings

--- Client/src/components/SearchBar.jsx
+++ Client/src/components/SearchBar.jsx
@@ -1,22 +1,26 @@
-import React, { useEffect, useState } from "react";
-import './SearchBar.css'
+import React, { useState } from "react";
+import "./SearchBar.css";
 
-export default function SearchBar({onSearch}) {
-  const [city, setCity] = useState('');
+export default function SearchBar({ onSearch }) {
+  const [city, setCity] = useState("");
 
   return (
-    <form className='form-conteiner' onSubmit={(e) => {
-      e.preventDefault();
-      onSearch(city);
-      setCity('');
-    }}>
-      <input className = 'elinput'
+    <form
+      className="form-conteiner"
+      onSubmit={(e) => {
+        e.preventDefault();
+        onSearch(city);
+        setCity("");
+      }}
+    >
+      <input
+        className="elinput"
         type="text"
         placeholder="Clima en..."
-        value = {city}
-        onChange = {e => setCity(e.target.value)}
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
       />
-      <input className = 'elboton' type="submit" value="Agregar" />
+      <input className="elboton" type="submit" value="Agregar" />
     </form>
-  ); 
+  );
 }
